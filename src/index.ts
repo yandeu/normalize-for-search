d35227f0a1a1d540044325e41b240a075e9d0df5
yandeu <20306025+yandeu@users.noreply.github.com>
2022-03-16 17:23:41 +0100

    .

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+/**
+ * @author       Yannick Deubel (https://github.com/yandeu)
+ * @copyright    Copyright (c) 2016 Ivan Krechetov
+ * @copyright    Copyright (c) 2022 Yannick Deubel
+ * @license      {@link https://github.com/ikr/normalize-for-search#license-mit | MIT}
+ * @license      {@link https://github.com/yandeu/TODO | MIT}
+ */
+
+export const normalizeForSearch = (str: string) => {
+  function filter(c: string) {
+    switch (c) {
+      case 'æ':
+      case 'ä':
+        return 'ae'
+
+      case 'å':
+        return 'a' // 'aa'
+
+      case 'á':
+      case 'à':
+      case 'ã':
+      case 'â':
+        return 'a'
+
+      case 'ç':
+      case 'č':
+        return 'c'
+
+      case 'é':
+      case 'ê':
+      case 'è':
+      case 'ë':
+      case 'ē':
+        return 'e'
+
+      case 'î':
+      case 'ï':
+      case 'í':
+        return 'i'
+
+      case 'œ':
+      case 'ö':
+        return 'oe'
+
+      case 'ó':
+      case 'õ':
+      case 'ô':
+        return 'o'
+
+      case 'ś':
+      case 'š':
+        return 's'
+
+      case 'ü':
+        return 'ue'
+
+      case 'ù':
+      case 'ú':
+      case 'ŭ':
+        return 'u'
+
+      case 'ß':
+        return 'ss'
+
+      case 'ё':
+        return 'e'
+
+      default:
+        return c
+    }
+  }
+
+  let normalized = ''
+  str = str.toLowerCase()
+  str = str.trim()
+
+  for (let i = 0, l = str.length; i < l; i = i + 1) {
+    normalized = normalized + filter(str.charAt(i))
+  }
+
+  // replace ae, oe, ue
+  normalized = normalized.replace(/ae/g, 'a')
+  normalized = normalized.replace(/oe/g, 'o')
+  normalized = normalized.replace(/ue/g, 'u')
+
+  return normalized
+}
---
+++ test/simple.test.js
@@ -0,0 +1,26 @@
+const { normalizeForSearch } = require('../lib/index')
+
+test('to lowercase', () => {
+  expect(normalizeForSearch('Yannick')).toBe('yannick')
+})
+
+test('trim', () => {
+  const result = 'yannick'
+  expect(normalizeForSearch(' Yannick')).toBe(result)
+  expect(normalizeForSearch('Yannick ')).toBe(result)
+})
+
+test('umlaut', () => {
+  const result = 'zmorgele'
+  expect(normalizeForSearch('Zmörgele')).toBe(result)
+  expect(normalizeForSearch('Zmorgele')).toBe(result)
+  expect(normalizeForSearch('Zmoergele')).toBe(result)
+})
+
+test('accents', () => {
+  const result = 'jerome'
+  expect(normalizeForSearch('Jérôme')).toBe(result)
+  expect(normalizeForSearch('jeroeme')).toBe(result)
+  expect(normalizeForSearch('Jèröme')).toBe(result)
+  expect(normalizeForSearch('Jerome')).toBe(result)
+})
