54d8907cceaa59c3572663c6dc72b76a73745b7a
yandeu <20306025+yandeu@users.noreply.github.com>
2022-03-18 14:59:49 +0100

    Simplify customNormalize

--- src/index.ts
+++ src/index.ts
@@ -7,63 +7,16 @@
  */
 
 export const normalizeForSearch = (str: string) => {
-  function filter(c: string) {
+  function customNormalize(c: string) {
     switch (c) {
       case 'æ':
-      case 'ä':
         return 'ae'
 
-      case 'å':
-        return 'a' // 'aa'
-
-      case 'á':
-      case 'à':
-      case 'ã':
-      case 'â':
-        return 'a'
-
-      case 'ç':
-      case 'č':
-        return 'c'
-
-      case 'é':
-      case 'ê':
-      case 'è':
-      case 'ë':
-      case 'ē':
-        return 'e'
-
-      case 'î':
-      case 'ï':
-      case 'í':
-        return 'i'
-
       case 'œ':
-      case 'ö':
         return 'oe'
-
-      case 'ó':
-      case 'õ':
-      case 'ô':
-        return 'o'
-
-      case 'ś':
-      case 'š':
-        return 's'
-
-      case 'ü':
-        return 'ue'
-
-      case 'ù':
-      case 'ú':
-      case 'ŭ':
-        return 'u'
 
       case 'ß':
         return 'ss'
-
-      case 'ё':
-        return 'e'
 
       default:
         return c
@@ -81,7 +34,7 @@ export const normalizeForSearch = (str: string) => {
 
   // custom normalization
   for (let i = 0, l = str.length; i < l; i = i + 1) {
-    normalized = normalized + filter(str.charAt(i))
+    normalized = normalized + customNormalize(str.charAt(i))
   }
 
   // replace ae, oe, ue
